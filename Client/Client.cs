3ec65cb3a0bc56ccdcd32c4593e15844a03918ec
Дмитрий <@DESKTOP-B26T9UQ>
2019-03-01 13:16:14 +0400

    da

--- Client/Client.cs
+++ Client/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -66,28 +67,49 @@ namespace Client
 
         }
 
+        public static void Compile(string program)
+        {
+            string writePath = "test";
+            StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default);
+            sw.WriteLine(program);
+            sw.Close();
+            runCmd(@"""C:\Program Files\Java\jdk-11.0.2\bin\javac.exe""" + @"""D:\Projects\C#\IDE\Client\bin\Debug\" + writePath + @".java""");
+            runCmd(@"""C:\Program Files\Java\jdk-11.0.2\bin\java.exe""" + @" -classpath D:\Projects\C#\IDE\Client\bin\Debug\ " + writePath);
+        }
+
+        private static void runCmd(string commands)
+        {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "cmd.exe",
+                    RedirectStandardInput = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                }
+            };
+            process.Start();
+
+            using (StreamWriter pWriter = process.StandardInput)
+            {
+                if (pWriter.BaseStream.CanWrite)
+                {
+                    foreach (var line in commands.Split('\n'))
+                        pWriter.WriteLine(line);
+                }
+            }
+            StreamReader sr = process.StandardOutput;
+            StringBuilder sb = new StringBuilder();
+            while (!sr.EndOfStream)
+            {
+                sb.Append(sr.ReadLine()).Append("\n");
+            }
+            MessageBox.Show(sb.ToString());
+        }
+
         public static string ReceiveMessage()
         {
-            /*StringBuilder SB = new StringBuilder();
-            int bytes = 0;
-            byte[] buffer = new byte[256];
-
-            try
-            {
-                do
-                {
-                    bytes = client.Receive(buffer);
-                    SB.Append(Encoding.Unicode.GetString(buffer));
-                }
-                while (client.Available > 0);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-                client.Close();
-            }
-
-            return SB.ToString();*/
             string message = reader.ReadString();
             return message;
         }
