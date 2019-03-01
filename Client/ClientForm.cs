c4e9b5ee0fad078e8c82fe8b3386d93401e62c9a
Дмитрий <@DESKTOP-B26T9UQ>
2019-03-01 22:15:27 +0400

    dsa

--- Client/ClientForm.cs
+++ Client/ClientForm.cs
@@ -1,70 +1,141 @@
 ﻿using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Client
 {
     public partial class ClientForm : Form
     {
+        private Client client;
         public ClientForm()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void ConnectButton_Click(object sender, EventArgs e)
         {
-            Client.Connect(textBox2.Text);
-            Thread thread = new Thread(Working);
-            thread.Start();
+            client = new Client(ipTextBox.Text, codeTextBox);
         }
 
-        private void Working()
+        private void compileButton_Click(object sender, EventArgs e)
         {
-            while (true)
+
+        }
+
+        private void KeyPressCodeTextBox(object sender, KeyPressEventArgs e)
+        {
+            client.KeyPressEvent(codeTextBox.Text);
+        }
+
+
+        class Client
+        {
+            private Socket client;
+            private NetworkStream stream;
+            private BinaryReader reader;
+            private BinaryWriter writer;
+            private bool connected = false;
+            private TextBox textBox;
+
+            public Client(string ip, TextBox textBox)
             {
-                string message = Client.ReceiveMessage();
-                JSON json = JsonConvert.DeserializeObject<JSON>(message);
+                int port = 228;
+                Connect(ip, port);
+                this.textBox = textBox;
+            }
+
+            private void Connect(string ip, int port)
+            {
+                try
+                {
+                    client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    client.Connect(new IPEndPoint(IPAddress.Parse(ip), port));
+                    stream = new NetworkStream(client);
+                    reader = new BinaryReader(stream);
+                    writer = new BinaryWriter(stream);
+                    connected = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    stream.Close();
+                    reader.Close();
+                    writer.Close();
+                    client.Close();
+                    return;
+                }
+
+                Thread thread = new Thread(Working);
+                thread.Start();
+            }
+
+            private void Working()
+            {
+                while (true)
+                {
+                    string message = ReceiveMessage();
+                    JSON json = JsonConvert.DeserializeObject<JSON>(message);
+                    WriteToTextBox(json.Data);
+                    JsonParse(json);
+                }
+            }
+
+            private void JsonParse(JSON json)
+            {
                 switch (json.Type)
                 {
                     case JSONType.text:
-                        WriteTextToTextBox(json.Data);
+                        WriteToTextBox(json.Data);
                         break;
                     case JSONType.cmd:
                         MessageBox.Show(json.Data);
                         break;
                 }
-                
+            }
+
+            private void WriteToTextBox(string text)
+            {
+                textBox.Invoke((MethodInvoker)delegate
+                {
+                    textBox.Text = text;
+                });
+            }
+
+            public bool Connected()
+            {
+                return connected;
+            }
+
+            public void SendMessage(string message)
+            {
+                writer.Write(message);
+            }
+
+            public string ReceiveMessage()
+            {
+                string message = reader.ReadString();
+                return message;
+            }
+
+            public void ButtonCompiteClickEvent(string text)
+            {
+                JSON json = new JSON(JSONType.compile, text);
+                string j = JsonConvert.SerializeObject(json);
+                SendMessage(j);
+            }
+
+            public void KeyPressEvent(string text)
+            {
+                JSON json = new JSON(JSONType.text, text);
+                string j = JsonConvert.SerializeObject(json);
+                SendMessage(j);
             }
         }
 
-        private void WriteTextToTextBox(string text)
-        {
-            textBox1.Invoke((MethodInvoker)delegate
-            {
-                textBox1.Text = text;
-            });
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            JSON json = new JSON(JSONType.compile, textBox1.Text);
-            string j = JsonConvert.SerializeObject(json);
-            Client.SendMessage(j);
-        }
-
-        private void KeyPressTextBox1(object sender, KeyPressEventArgs e)
-        {
-            JSON json = new JSON(JSONType.text, textBox1.Text);
-            string j = JsonConvert.SerializeObject(json);
-            Client.SendMessage(j);
-        }
+       
     }
 }
