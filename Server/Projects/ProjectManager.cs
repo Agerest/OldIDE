ec566fc95476bf0b431852027d13a4e405b49a7c
Дмитрий <@DESKTOP-B26T9UQ>
2019-03-21 20:39:58 +0400

    Fix

--- Server/Projects/ProjectManager.cs
+++ Server/Projects/ProjectManager.cs
@@ -3,18 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace Server.Projects
 {
     static class ProjectManager
     {
-        private const string XML_FILE_PATH = @"Properties\ProjectManager.xml";//Пофиксить
+        private const string XML_FILE_PATH = @"Properties\ProjectManager.xml";
 
         private static List<Project> projects;
+        private static XmlDocument xml;
 
         public static void ReadXml()
         {
+            xml.Load(XML_FILE_PATH);
+            XmlElement nodeList = xml.DocumentElement;
+            foreach (var v in nodeList)
+            {
 
+            }
+            int ID ;
+            string name;
+            string xmlFilePath;
         }
 
         public static Project GetProject(long ID)
