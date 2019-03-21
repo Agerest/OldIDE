7cb3b0d247e486d77a8f0965d2b40b7189aa025d
Сергей Владимирович Крес <160257@edu.psuti.ru>
2019-03-21 18:39:28 +0300

    Fix

--- Server/ProjectStruct/Project.cs
+++ Server/ProjectStruct/Project.cs
@@ -1,27 +1,23 @@
 ﻿using Newtonsoft.Json;
 using Server.JSON;
-using Server.ProjectStruct;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Server
 {
     class Project
     {
-        private List<File> classes;
+        private const string XML_PATH = "";
+        private List<ProjectStruct.File> classes;
         private string Name { get; set; }
         private int ID { get; set; }
 
         public Project()
         {
-            classes = new List<File>();
+            classes = new List<ProjectStruct.File>();
         }
 
-        public void AddClass(File _class)
+        public void AddClass(ProjectStruct.File _class)
         {
             classes.Add(_class);
         }
@@ -29,7 +25,7 @@ namespace Server
         public string Serialize()
         {
             List<JsonClass> jsonClasses = new List<JsonClass>();
-            foreach (File file in classes)
+            foreach (ProjectStruct.File file in classes)
             {
                 jsonClasses.Add(file.ToJsonClass());
             }
@@ -37,35 +33,12 @@ namespace Server
             return JsonConvert.SerializeObject(jsonProject);
         }
 
-        private static void CreateFileNodes()// Нужно перенести на сервер
+        public void OpenProject()
         {
-            DirectoryInfo rootDirectory = new DirectoryInfo(path);
-            TreeNode rootNode = new TreeNode(name);
-            FillNodes(rootDirectory, rootNode);
-            treeView.Nodes.Add(rootNode);
+
         }
 
-        private static void FillNodes(DirectoryInfo DI, TreeNode parentNode)
-        {
-            TreeNode node = new TreeNode(DI.Name);
-            FileInfo[] files = DI.GetFiles();
-
-            foreach (FileInfo file in files)
-            {
-                node.Nodes.Add(file.Name);
-            }
-
-            parentNode.Nodes.Add(node);
-
-            DirectoryInfo[] directories = DI.GetDirectories();
-
-            foreach (DirectoryInfo directory in directories)
-            {
-                FillNodes(directory, node);
-            }
-
-
-        }
+        
 
     }
 }
